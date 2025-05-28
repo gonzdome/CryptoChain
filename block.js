4691cf106489c4306ceb4bac23597861b10da5a0
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-28 17:59:52 -0300

    feat: add mineBlock method

--- block.js
+++ block.js
@@ -7,7 +7,7 @@ class Block {
      * @param {string} lastHash - The hash of the previous block.
      * @param {string} hash - The hash of this block.
      * @param {any} data - The data stored in the block.
-     */
+    */
     constructor({ timestamp, lastHash, hash, data }) {
        this.timestamp = timestamp; 
        this.lastHash = lastHash; 
@@ -18,11 +18,23 @@ class Block {
     /**
      * Factory method: Creates and returns the genesis block.
      * @returns {Block} The genesis block.
-     */
+    */
     static genesis() {
         return new this(GENESIS_DATA);
     };
 
+    /**
+     * Factory Method: Static function to mine a new block
+     * @param {Block} lastBlock - The last block in the chain.
+     * @param {any} data - The data to store in the new block.
+     * @returns {Block} The newly mined block.
+    */
+    static mineBlock({ lastBlock, data }) {
+        return new this({
+            timestamp: Date.now(),
+            lastHash: lastBlock.hash,
+            data
+        });
     }
 }
 
