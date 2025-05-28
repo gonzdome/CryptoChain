b47c07d9431f3f5a83aa7dac33cac926f2adcf18
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-28 18:00:21 -0300

    feat: add crypto-hash tests

---
+++ tests/crypto-hash.test.js
@@ -0,0 +1,15 @@
+const cryptoHash = require('../crypto-hash');
+
+describe('cryptoHash', () => {
+    const hashFoo = '2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae';
+
+    describe('generates a SHA-256 hashed output', () => {
+        it('returns the Hashed data', () => {
+            expect(cryptoHash('foo')).toEqual(hashFoo);
+        });
+
+        it('produces the same hash with the same input arguments in any order', () => {
+            expect(cryptoHash('one', 'two', 'three')).toEqual(cryptoHash('two', 'three', 'one'));
+        });
+    })
+});
