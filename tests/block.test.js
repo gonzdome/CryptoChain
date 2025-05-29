68cffa24aae794ea3834acaaa3b997f5b1ce3d95
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-28 21:59:16 -0300

    feat: add test for hash on block test

--- tests/block.test.js
+++ tests/block.test.js
@@ -1,4 +1,5 @@
 const Block = require("../block");
+const cryptoHash = require("../crypto-hash");
 const { GENESIS_DATA } = require("../config");
 
 describe('Block', () => {
@@ -48,6 +49,10 @@ describe('Block', () => {
 
         it('sets the `timestamp`', () => {
             expect(minedBlock.data).not.toEqual(undefined);
+        })
+
+        it('creates a SHA-256 `hash` based on the proper inputs', () => {
+            expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data));
         })
     });
 });
