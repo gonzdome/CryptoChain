62c416fcb1354105589827964729bbb532dc871a
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-06-08 22:12:12 -0300

    feat: add post /api/mine route and its functionality

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 
 const app = express();
 const blockchain = new Blockchain();
 
-app.get('/api/blocks', (request, response) => {
-    response.json(blockchain.chain);
+app.use(bodyParser.json());
+
+app.get('/api/blocks', (req, res) => {
+    res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req, res) => {
+    const { data } = req.body;
+    
+    blockchain.addBlock({ data });
+
+    res.redirect('/api/blocks');
 });
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`listening at localhost: ${PORT}`)
+    console.log(`listening at http://localhost:${PORT}`)
 });
