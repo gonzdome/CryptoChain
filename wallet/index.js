4a7f689b311d2ba849a6a615d315c8e9e2f94451
Gabriel <gabrielgonzalezdomeniconi@gmail.com>
2025-06-17 16:39:04 -0300

    feat: add sign function and necessary changes

--- wallet/index.js
+++ wallet/index.js
@@ -1,19 +1,26 @@
 const { STARTING_BALANCE } = require("../config");
 const { ec } = require("../utils/elliptic");
+const cryptoHash = require("../utils/crypto-hash");
+
 
 class  Wallet {
     /**
-     * 
+     * Wallet class
      */
     constructor() {
         /** @property {int} balance - Balance of the wallet. */
         this.balance = STARTING_BALANCE;
 
-        const keyPair = ec.genKeyPair();
+        this.keyPair = ec.genKeyPair();
         
         /** @property {any} publicKey - Public Key. */
-        this.publicKey = keyPair.getPublic().encode('hex');
-    }
+        this.publicKey = this.keyPair.getPublic().encode('hex');
+    };
+
+    sign(data) {
+        const hashedData = cryptoHash(data);
+        return this.keyPair.sign(hashedData);
+    };
 };
 
 module.exports = Wallet;
