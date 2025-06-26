8f470d49808ba0b9b6225ef4b1fd7fb66b5e3b87
Gabriel <gabrielgonzalezdomeniconi@gmail.com>
2025-06-26 13:32:58 -0300

    refactor: removed const used once and placed new value on test directly

--- utils/tests/crypto-hash.test.js
+++ utils/tests/crypto-hash.test.js
@@ -1,11 +1,9 @@
 const cryptoHash = require('../crypto-hash');
 
 describe('cryptoHash', () => {
-    const hashFoo = '2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae';
-
     describe('generates a SHA-256 hashed output', () => {
         it('returns the Hashed data', () => {
-            expect(cryptoHash('foo')).toEqual(hashFoo);
+            expect(cryptoHash('foo')).toEqual('b2213295d564916f89a6a42455567c87c3f480fcd7a1c15e220f17d7169a790b');
         });
 
         it('produces the same hash with the same input arguments in any order', () => {
