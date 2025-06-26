c71ce97544413317f68bc46c924608eea11ffcee
Gabriel <gabrielgonzalezdomeniconi@gmail.com>
2025-06-26 13:34:17 -0300

    refactor: add JSON.stringfy inside map on cryptoHash( ) to avoid js object behavior

--- utils/crypto-hash.js
+++ utils/crypto-hash.js
@@ -8,7 +8,7 @@ const crypto = require('crypto');
 const cryptoHash = (...inputs) => {
     const hash = crypto.createHash('sha256');
 
-    const data = inputs.sort().join(' ');
+    const data = inputs.map(input => JSON.stringify(input)).sort().join(' ');
 
     hash.update(data);
 
