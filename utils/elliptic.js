5db55c9c068027bd0324e5d12f36ed99e1b41b9a
Gabriel <gabrielgonzalezdomeniconi@gmail.com>
2025-06-27 12:27:51 -0300

    refactor: add JSDoc to elliptic -> verifySignature( ) and remove empty line

--- utils/elliptic.js
+++ utils/elliptic.js
@@ -3,9 +3,15 @@ const EC = require('elliptic').ec;
 
 const ec = new EC('secp256k1');
 
+/**
+ * Verifies the signature.
+ * @param {any} publicKey - Generaly a string
+ * @param {any} data - Any data
+ * @param {object} signature - The signature.
+ * @returns {boolean} - If the signature is valid or not.
+ */
 const verifySignature = ({ publicKey, data, signature }) => {
     const keyFromPublic = ec.keyFromPublic(publicKey, 'hex');
-
     return keyFromPublic.verify(cryptoHash(data), signature);
 }
 
