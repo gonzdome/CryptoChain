9526c85a4dab2ccb656424842578e621abd6f86f
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-07-01 15:50:50 -0300

    feat: add transactionPool to the instance of PubSub class

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({ blockchain });
+const pubsub = new PubSub({ blockchain, transactionPool });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
