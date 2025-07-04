f0c7ebe715be19790ecf5f8b75aa3a5bc140e634
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-07-04 19:50:49 -0300

    feat: add transaction-pool -> clear( ) and clearBlockchainTransactions( )

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -39,6 +39,29 @@ class TransactionPool {
     validTransactions() {
         return Object.values(this.transactionMap).filter(transaction => Transaction.validTransaction(transaction));
     };
+
+    /**
+     * Clears the transactionMap by setting it to an empty object.
+    */
+    clear() {
+        this.transactionMap = {};
+    }
+
+    /**
+     * Removes from the transaction pool any transactions that have already been included in the blockchain.
+     * Iterates through each block in the chain (except the genesis block) and deletes transactions from the pool if their IDs are found in the block's data.
+     * @param {Object} options - The options object.
+     * @param {Array} options.chain - The blockchain array to check for existing transactions.
+    */
+    clearBlockchainTransactions({ chain }) { 
+        for (let i = 1; i < chain.length; i++) {
+            const block = chain[i];
+
+            for (let transaction of block.data) {
+                if (this.transactionMap[transaction.id]) delete this.transactionMap[transaction.id];
+            };
+        };
+    };
 };
 
 module.exports = TransactionPool;
