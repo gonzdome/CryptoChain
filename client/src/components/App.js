05b7b9d99e4ec057b54707854dd48f77e08335e3
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2025-07-15 16:27:19 -0300

    feat: create first component App

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                CryptoChain in React!
+
+                Welcome to the Blockchain...
+            </div>
+        );
+    }
+};
+
+export default App;
