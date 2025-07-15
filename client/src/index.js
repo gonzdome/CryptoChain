eefb963af9a05174877bf1d34e8e08cf032d9275
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2025-07-15 16:27:42 -0300

    feat: call App inside client/src/index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/App';
 
 
 render(
-    <div>
-        CryptoChain in React!
-    </div>,
+    <App />,
     document.getElementById('root')
 );
