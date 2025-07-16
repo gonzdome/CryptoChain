bf080009cfd2ae754ccba8a1e360d8aac432707d
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2025-07-16 16:19:01 -0300

    feat: improvement on App.js, added state, componentDidMount and some text

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 
 class App extends Component {
+    state = { walletInfo: {} };
+
+    componentDidMount() {
+        fetch(`${process.env.URL}${process.env.PORT}/api/wallet-info`)
+            .then(response => response.json())
+            .then(walletInfo => this.setState({ walletInfo }));
+    };
+
+
     render() {
+        const { address, balance } = this.state.walletInfo;
+
         return (
             <div>
-                CryptoChain in React!
+                <div>
+                    <h3>Welcome to the Blockchain!</h3>
+                </div>
 
-                Welcome to the Blockchain...
+
+                <div>Address: {address}</div>
+                <div>Balance: {balance}</div>
+                <br />
+                <div>(This is a demo, not a real wallet)</div>
             </div>
         );
     }
