12c1ec2e2343b0b04f383fd28b2f2a4d816c8752
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2025-07-18 16:54:44 -0300

    feat: create logic on the Block component to show or hide transactions data

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 class Block extends Component {
-    render() {
-        const { timestamp, hash, data } = this.props.block;
+    state = { displayTransaction: false };
 
-        const hashData = hash ? hash : 'No hash available';
-        const hashDisplay = hashData.length > 15 ?
-            `${hashData.substring(0, 15)}...` : hashData;
+    toggleTransactionDisplay = () => {
+        this.setState({ displayTransaction: !this.state.displayTransaction });
+    };
+
+    get displayTransaction() {
+        const { data } = this.props.block;
 
         const stringfiedData = data ? JSON.stringify(data) : 'No data available';
         const dataDisplay = stringfiedData.length > 35 ?
             `${stringfiedData.substring(0, 35)}...` : stringfiedData;
 
+        return <div>
+            Data: {this.state.displayTransaction ? JSON.stringify(data) : dataDisplay}
+            <br />
+            <Button
+                bsStyle="danger"
+                bbSize="small"
+                onClick={this.toggleTransactionDisplay}
+            >
+                {this.state.displayTransaction ? 'Hide' : 'Show More'}
+            </Button>
+        </div>
+    };
+
+    render() {
+        const { timestamp, hash } = this.props.block;
+
+        const hashData = hash ? hash : 'No hash available';
+        const hashDisplay = hashData.length > 15 ?
+            `${hashData.substring(0, 15)}...` : hashData;
+
         return (
             <div className="Block">
                 <div>Hash: {hashDisplay}</div>
                 <div>Timestamp: {new Date(timestamp).toLocaleDateString()}</div>
-                <div>Data: {dataDisplay}</div>
+                {this.displayTransaction}
             </div>
         );
     };
