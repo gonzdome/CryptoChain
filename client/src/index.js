e97dc06dba1e6204fc96fded78ad913673e7c5a2
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2025-07-26 00:13:40 -0300

    feat: add Route to /transaction-pool

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import App from './components/App';
 import Blocks from './components/Blocks';
 import ConductTransaction from './components/ConductTransaction';
 import './index.css';
+import TransactionPool from './components/TransactionPool';
 
 render(
     <Router history={history}>
@@ -13,6 +14,7 @@ render(
             <Route exact path="/" component={App} />
             <Route path="/blocks" component={Blocks} />
             <Route path="/transactions" component={ConductTransaction} />
+            <Route path="/transaction-pool" component={TransactionPool} />
         </Switch>
     </Router>,
     document.getElementById('root')
